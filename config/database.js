c4f93a46c535eda0591621a562ebbd9c0c72c403
Guxta010 <gdsdo77@gmail.com>
2025-04-07 01:17:32 -0300

    usando variaveis do ambiente na conexão com o banco

--- config/database.js
+++ config/database.js
@@ -1,14 +1,21 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-// Conexão com MySQL via Sequelize
-const sequelize = new Sequelize("assistencia_tecnica", "admin", "102030gg", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-});
+// Conexão com MySQL via Sequelize usando variáveis de ambiente
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
+// Testar conexão
 sequelize.authenticate()
   .then(() => console.log("Conexão com MySQL estabelecida! ✅"))
   .catch(err => console.error("Erro ao conectar no MySQL:", err));
 
-module.exports = sequelize;
+module.exports = sequelize;
